7e847c0b5cfb65018fd0be86140186384cb4127d
Gimban (Laptop) <sprtmsfhtk@naver.com>
2025-03-27 11:39:11 +0900

    광원 범위 자동 조절 기능 추가, 광원 프리팹에 컴포넌트로 추가, 임시 테스트 검증됨

--- test_place/Assets/AutoLight.cs
+++ test_place/Assets/AutoLight.cs
@@ -3,28 +3,28 @@ using UnityEngine;
 public class AutoLight : MonoBehaviour
 {
     public GameObject map_root;
+    private Transform root_tf;
     Light light_setting;
     float intensity;
     float range;
-    float[] root_scale;
+    float root_scale;
     void Start()
     {
         light_setting = GetComponent<Light>();
         intensity = light_setting.intensity;
         range = light_setting.range;
 
+        if (map_root == null)
+        {
+            map_root = GameObject.Find("Generated Map");
+            root_tf = map_root.GetComponent<Transform>();
+        }
+
         if (map_root != null)
         {
-            for (int i = 0; i < 3; i++)
-            {
-                root_scale[i] = map_root.transform.localScale[i];
-            }
-        }
-    }
+            root_scale = (root_tf.localScale.x + root_tf.localScale.y + root_tf.localScale.z) / 3;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            light_setting.range *= root_scale;
+        }
     }
 }
